e7149f2a5b57826a78196b9839cdb78e2b780d0e
彭浪 <3039654617@qq.com>
2021-11-06 13:44:57 +0800

    fix(定制全部功能的临时提交版本): 定制全部功能的临时提交版本
    定制全部功能的临时提交版本

--- src/layouts/components/AllFunction/index.tsx
+++ src/layouts/components/AllFunction/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 
 import './index.less';
+import Alphabetize from './components/Alphabetize';
 
 const AllFunction: React.FC<{
   data: any;
 }> = (props) => {
+  const [way, setWay] = useState('Alphabetize');
   console.log(props);
 
   return (
@@ -16,10 +18,25 @@ const AllFunction: React.FC<{
           <Input size="middle" placeholder="请输入关键词进行搜索" prefix={<SearchOutlined />} />
         </div>
         <div className="two-way">
-          <div className="one">全部功能</div>
-          <div className="two">按字母检索</div>
+          <div
+            className="one"
+            onClick={() => {
+              setWay('Waterfall');
+            }}
+          >
+            全部功能
+          </div>
+          <div
+            className="two"
+            onClick={() => {
+              setWay('Alphabetize');
+            }}
+          >
+            按字母检索
+          </div>
         </div>
       </div>
+      <div className="content">{way === 'Alphabetize' && <Alphabetize />}</div>
     </div>
   );
 };
