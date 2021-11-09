de5f148eeb48bda848e138f5d2387af209cbffe4
彭浪 <3039654617@qq.com>
2021-11-09 16:20:04 +0800

    fix(制单临时提交版本): 制单临时提交版本
    制单临时提交版本

---
+++ src/layouts/components/MakeList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.less';
+
+const MakeList: React.FC<{
+  data: any;
+}> = (props) => {
+  console.log(props);
+
+  return <div className="makelist-wrap">fff</div>;
+};
+
+export default MakeList;
